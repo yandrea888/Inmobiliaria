c38b5298608c39f131dc465d765c3b8ff4e2ba02
yandrea888 <yandream888@gmail.com>
2024-12-12 17:42:18 -0500

    Correcciones #2 Data base RigoRigo

--- obj/Debug/net8.0/Inmobiliaria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Inmobiliaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fac2bad7e3eb72c3de1732f622c881c640ae472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436937ed1947069c6a7778f162fd27377606ab3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
